0355449d41687abd4ede58463dd3700e2e64e2c9
ntswaki1934 <ntswaki1934@gmail.com>
2023-11-13 06:53:05 -0800

    memory allocation to the shell

--- hsh.c
+++ hsh.c
@@ -9,27 +9,62 @@
 
 int main(int ni, char **argv)
 {
-	char *prompt = "NtswakImane $";
+	char *prompt = ("NtswakImane $");
+	size_t n = 0;
+	ssize_t chars_read;
 	const char *delim = "\n";
 	char *lineptr = NULL, *lineptr_copy = NULL;
-	size_t n = 0;
-	ssize_t chars_read;	
-
-	(void)ni; (void)argv;
+	char *tokens;
+	int tokens_read;
+	int j;
+	
+	(void)ni;
+	
 
 	while(1)
 	{
 		printf("%s", prompt);
-	chars_read = getline(&lineptr, &n, stdin);
-	if(chars_read == -1)
-	{
-		printf("exit\n");
-		return(-1);
-	}
+		chars_read = getline(&lineptr, &n, stdin);
+
+		/**getline() function called**/
+
+		if(chars_read == -1)
+		{
+			printf("exit\n");
+			return (-1);
+		}
+		lineptr_copy = malloc(sizeof(char) *chars_read);
+		if(lineptr_copy == NULL)
+		{
+			perror("tsh: memory allocation error");
+			return (-1);
+		}
+		strcpy(lineptr_copy, lineptr);
+		tokens = strtok(lineptr, delim);
+
+		while(tokens != NULL)
+		{
+			tokens_read++;
+			tokens = strtok(NULL, delim);
+		}
+		tokens_read++;
+
+		argv = malloc(sizeof(char *) * chars_read);
+		tokens = strtok(lineptr_copy, delim);
+		for (j = 0; tokens != NULL; j++)
+		{
+			argv[j] = malloc(sizeof(char) * strlen(tokens));
+			strcpy(argv[j], tokens);
+
+			tokens = strtok(NULL, delim);
+		}
+		argv[j] = NULL;
+
+
 	printf("%s", lineptr);
 
 	free(lineptr);
-	
-	return(0);
 	}
+
+	return (0);
 }
